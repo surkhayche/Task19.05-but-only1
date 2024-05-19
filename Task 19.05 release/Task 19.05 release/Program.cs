48f4e1628d66b5651a075fcc07bd384fea5a43e1
Surkhay <surkhayshikhiyev@gmail.com>
2024-05-19 22:17:56 +0400

    Added task 19.05

---
+++ Task 19.05 release/Task 19.05 release/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_19._05_release
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string sentence = "Salam Aleykum ";             
+            //UpperCaseMethod(sentence);
+        }
+        //static void UpperCaseMethod (string sentence)
+        //{
+        //    bool newSentence = true;
+        //    for (int i = 0; i < sentence.Length; i++)
+        //    {
+        //        char charecter = sentence[i];
+        //        if (charecter == ' ')
+        //        {
+        //            newSentence = true;
+        //        }
+        //        else if (newSentence)
+        //        {
+        //            Console.WriteLine(charecter + " ");
+        //            newSentence = false;
+        //        }
+        //    }
+        //}
+    }
+}
+
